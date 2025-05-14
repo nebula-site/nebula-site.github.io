335f8ddd8f72489336d0c2c83e02da9e8b0904f1
nebula-site <nebula.site1@outlook.com>
2025-05-14 15:33:55 -0600

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -3,8 +3,8 @@ document.getElementById('navbar-element').innerHTML = `
     <div class="navbar">
       <div class="logo"><a href="/index">Nebula</a></div>
       <ul class="menu">
-        <li><a href="/index"><i class="fa-solid fa-house"></i>Home</a></li>
-        <li><a href="/games"><i class="fa-solid fa-gamepad"></i>Games</a></li>
+        <li><a href="/index"><i class="fa-solid fa-house"></i> Home</a></li>
+        <li><a href="/games"><i class="fa-solid fa-gamepad"></i> Games</a></li>
       </ul>
       <div class="search-box">
         <input type="text" placeholder="Search here...">
