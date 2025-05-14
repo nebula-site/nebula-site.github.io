8aea51c13591d38e0fb3ec6a0946f969017e35e2
nebula-site <nebula.site1@outlook.com>
2025-05-14 17:57:14 -0600

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -8,8 +8,8 @@ document.getElementById('navbar-element').innerHTML = `
     <div class="navbar">
       <div class="logo"><a href="/index.html">Nebula</a></div>
       <ul class="menu">
-        <li><a href="/index.html"><i class="fa-solid fa-house"></i> Home</a></li>
-        <li><a href="/profile.html"><i class="fa-solid fa-user"></i> Profile</a></li>
+        <li><a href="/"><i class="fa-solid fa-house"></i> Home</a></li>
+        <li><a href="/games"><i class="fa-solid fa-gamepad"></i> Games</a></li>
       </ul>
       <div class="user-profile">
         <img src="${user.avatar}" alt="User Avatar" class="avatar">
