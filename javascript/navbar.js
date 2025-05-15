020d469be8d74ec5bffe478b402be982d3898b16
nebula-site <nebula.site1@outlook.com>
2025-05-14 18:29:50 -0600

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,7 +1,10 @@
 const user = JSON.parse(localStorage.getItem('user')) || {
   name: "Guest",
-  avatar: "/branding/favicon.png"
+  avatar: "/branding/favicon.png" // Default avatar
 };
+
+// Check if user avatar exists or is empty, and provide default
+const avatarSrc = user.avatar ? user.avatar : "/branding/favicon.png";
 
 document.getElementById('navbar-element').innerHTML = `
   <nav>
@@ -11,11 +14,13 @@ document.getElementById('navbar-element').innerHTML = `
         <li><a href="/"><i class="fa-solid fa-house"></i> Home</a></li>
         <li><a href="/games"><i class="fa-solid fa-gamepad"></i> Games</a></li>
       </ul>
-      <a href="/profile"><div class="user-profile">
-        <img src="${user.avatar}" class="avatar" href="/profile">
-        <button class="dropdown-toggle">${user.name}</button>
-        <span class="username">${user.name}</span>
-      </div></a>
+      <a href="/profile">
+        <div class="user-profile">
+          <img src="${avatarSrc}" class="avatar" alt="User Avatar">
+          <button class="dropdown-toggle">${user.name}</button>
+          <span class="username">${user.name}</span>
+        </div>
+      </a>
       <div class="menu-btn">
         <i class="fas fa-bars"></i>
       </div>
