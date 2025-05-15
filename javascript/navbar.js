2ffe80e8fd71ab1bf6dd1339da6220bf90cd120b
nebula-site <nebula.site1@outlook.com>
2025-05-14 18:25:22 -0600

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -11,11 +11,11 @@ document.getElementById('navbar-element').innerHTML = `
         <li><a href="/"><i class="fa-solid fa-house"></i> Home</a></li>
         <li><a href="/games"><i class="fa-solid fa-gamepad"></i> Games</a></li>
       </ul>
-      <div class="user-profile">
+      <a href="/profile"><div class="user-profile">
         <img src="${user.avatar}" alt="User Avatar" class="avatar" href="/profile">
         <button class="dropdown-toggle">${user.name}</button>
         <span class="username">${user.name}</span>
-      </div>
+      </div></a>
       <div class="menu-btn">
         <i class="fas fa-bars"></i>
       </div>
