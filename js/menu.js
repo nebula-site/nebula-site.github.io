895b45a346524e3bc1470c8b3826c06f4515782d
Nebula <contact.nebula.site@gmail.com>
2025-12-05 21:51:20 -0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
- import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 
 const supabase = createClient(
   'https://lhurtuuxsmlakoikcpiz.supabase.co',
